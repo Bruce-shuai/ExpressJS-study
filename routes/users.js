c64ba2210ff5059b5a8853aa89ac7dc4077045b0
帅得乱七八糟 <284970670@qq.com>
2021-11-20 22:11:13 +0800

    学习expressjs

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,81 @@
-const express = require('express');
-const router = express.Router(); 
+// const express = require("express")
+// const router = express.Router()
 
-router.get('/', (req, res) => {
-  res.send('User List')
-})
 
-router.get('/new', (req, res) => {
-  res.send('User New Form')
+// router.get("/", (req, res) => {
+//   res.send("这是User路径")
+// })
+// router.get("/new", (req, res) => {
+//   res.send("这是User 下 的new路径")
+// })
+
+// router.route("/:id")
+// .get((req, res) => {
+//   console.log(req.user)
+//   res.send(`Get User With ID ${req.params.id}`)
+// })
+// .post((req, res) => {
+//   res.send(`Post User With ID ${req.params.id}`)
+// })
+// .put((req, res) => {
+//   res.send(`Put User With ID ${req.params.id}`)
+// })
+// .delete((req, res) => {
+//   res.send(`Delete User With ID ${req.params.id}`)
+// })
+
+// const users = [{name: '帅得乱七八糟'}, {name: '帅得歪瓜裂枣'}]
+// router.param('id', (req, res, next, id) => {
+//   req.user = users[id];
+//   console.log(req.user);
+//   next();
+// })
+
+// module.exports = router;   // 注意：这里是exports
+
+
+const express = require("express")
+const router = express.Router()
+
+
+router.get("/", (req, res) => {
+  res.send(req.query.food)
+  // res.send("...")
 })
+const users = [{username: '帅得歪瓜裂枣'}, {username: '帅得一塌糊涂'}]
+
 
 router.post('/', (req, res) => {
-  res.send('Create User');
+  const isValid = true;
+  if(isValid) {
+    users.push({firstName: req.body.username})
+    res.redirect(`/users/${users.length - 1}`)
+  } else {
+    console.log('Error')
+    res.render('users/user', {username: req.body.username})
+  }
 })
 
-// router.get('/:id', (req, res) => {  // 不要自己手写1、2、3...而应该用id变量来代替
-//   // req.params.id   
-//   res.send(`Get User With ID ${req.params.id}`)
-// })
-
-// router.put('/:id', (req, res) => {  // 不要自己手写1、2、3...而应该用id变量来代替
-//   // req.params.id   
-//   res.send(`Get User With ID ${req.params.id}`)
-// })
-
-// router.delete('/:id', (req, res) => {  // 不要自己手写1、2、3...而应该用id变量来代替
-//   // req.params.id   
-//   res.send(`Get User With ID ${req.params.id}`)
-// })
-
-router
-  .route('/:id')
-  .get((req, res) => {
-    console.log(req.user);
-    res.send(`Get User With ID ${req.params.id}`)
-  })
-  .put((req, res) => {
-    res.send(`Update User With ID ${req.params.id}`)
-  })
-  .delete((req, res) => {
-    res.send(`Delete User With ID ${req.params.id}`)
-  })
-
-const users = [{name: '帅得乱七八糟'}, {name: '帅得歪瓜裂枣'}]
-router.param('id', (req, res, next, id) => {
-  // console.log(id);
-  req.user = users[id]
-  next();
+router.route("/:id")
+.get((req, res) => {
+  console.log(req.user)
+  res.send(`Get User With ID ${req.params.id}`)
+})
+.post((req, res) => {
+  res.send(`Post User With ID ${req.params.id}`)
+})
+.put((req, res) => {
+  res.send(`Put User With ID ${req.params.id}`)
+})
+.delete((req, res) => {
+  res.send(`Delete User With ID ${req.params.id}`)
 })
 
-module.exports = router;
+
+// router.post("/new", (req, res) => {
+//   res.render("users/new", {username: '帅得乱七八糟'})
+// })
+router.get("/new", (req, res) => {
+  res.render("users/new", {username: '帅得乱七八糟'})
+})
+module.exports = router;   
--- server.js
+++ server.js
@@ -1,35 +1,23 @@
-// console.log('Hi');
-const express = require('express')
+/* server.js */
+const express = require('express');
 const app = express();
 
 app.set('view engine', 'ejs')
-app.use(logger)
 
-app.get('/', (req, res) => {
-  console.log('Here');
-  // res.send('Hi')
-  // res.sendStatus(500)
-  // res.status(500).send('Hi')
-  // res.status(500).json({message: 'Error'})
-  // res.download('server.js')   // 直接会在客户端下载这个文件
-  res.render('index', {text: "World"})
+app.get('/',logger, (req, res) => {  // 我想作用于 '/' 路由
+  res.render('index',{text: 'World'})
 })
 
-// app.get('/users', (req, res) => {
-//   res.send('User List')
-// })
+app.use(express.static("public"))
+app.use(express.urlencoded({ extended: true}))
 
-// app.get('/users/new', (req, res) => {
-//   res.send('User New Form')
-// })
+const userRouter = require("./routes/users");
 
-const userRouter = require('./routes/users');
-app.use('/users', userRouter);
- 
+app.use("/users", userRouter);
+
 function logger(req, res, next) {
-  console.log(req.originalUrl);
+  console.log(req.originalUrl)
   next();
 }
 
-
-app.listen(3000);
+app.listen(3004);
